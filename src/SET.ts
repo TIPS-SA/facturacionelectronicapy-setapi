a63d7adb3328249a173e29c4ee6a1f07ca9fbbbc
Marcos Jara <adrianjara@gmail.com>
2021-10-07 16:07:41 -0300

    Correcciones

--- src/SET.ts
+++ src/SET.ts
@@ -1,8 +1,27 @@
 const soap = require('soap');
 //import soap from 'soap';
 import JSZip from "jszip";
+import pkcs12 from "./PKCS12";
+import fs from 'fs'; 
+import uri2path from 'file-uri-to-path';
 
 class SET {
+    private cert: any; 
+    private key: any;
+
+    /**
+     * Crea los certificados para authenticarse a la SET.
+     * @param certificado 
+     * @param passphase 
+     */
+    auth(certificado: any, passphase: string){
+        pkcs12.openFile(certificado, passphase);
+
+        //console.log("certificado", pkcs12.getCertificate());
+        //console.log("key", pkcs12.getPrivateKey());
+        this.cert = pkcs12.getCertificate();
+        this.key = pkcs12.getPrivateKey();
+    }
 
     /**
      * Envia el Documento electronico a la SET
@@ -32,6 +51,13 @@ class SET {
             url = 'https://sifen-test.set.gov.py/de/ws/async/recibe-lote.wsdl?wsdl';
         }
 
+        if (!this.cert) {
+            throw new Error("Antes debe Autenticarse");
+        }
+
+        if (!this.key) {
+            throw new Error("Antes debe autenticarse");
+        }
 
         const zip = new JSZip();
 
@@ -53,10 +79,42 @@ class SET {
                      </ns1:MyRootElement>"
             };*/
 
-        console.log("dato a enviar...", args);
+        //console.log("dato a enviar...", url, args);
 
-        console.log("soap", soap);
-        return soap.createClient(url, function(err: any, client: any) {
+        console.log("entro hasta aqui");
+        const oThis = this;
+        const wsdlPath = uri2path("file://localhost/" + __dirname + "/wsdl/recibe_lote.wsdl?wsdl");
+        console.log(wsdlPath);
+        return soap.createClient(wsdlPath, function(err: any, client: any) {
+            if (err) {
+                throw err;
+            }
+            //console.log("client", client);
+            
+            var certFName = __dirname + '/cert_' + oThis.generateRandom(1, 999999);
+            fs.writeFileSync(certFName, oThis.cert);
+
+            var keyFName = __dirname + '/key_' + oThis.generateRandom(1, 999999);
+            fs.writeFileSync(keyFName, oThis.key);
+
+            console.log(certFName, keyFName);
+            client.setSecurity(new soap.ClientSSLSecurity(
+                keyFName,
+                certFName,
+                //'/path/to/ca-cert',  /*or an array of buffer: [fs.readFileSync('/path/to/ca-cert/1', 'utf8'),
+                //'fs.readFileSync('/path/to/ca-cert/2', 'utf8')], */
+                {   /*default request options like */
+                    // strictSSL: true,
+                    // rejectUnauthorized: false,
+                    // hostname: 'some-hostname'
+                    //secureOptions: constants.SSL_OP_NO_TLSv1_2,
+                    // forever: true,
+                },
+            ));
+            fs.rm(certFName, function(e){});
+            fs.rm(keyFName, function(e){});
+
+            console.log("client");
             console.log("client", client);
             client.rEnvioLote(args, function(err: any, result: any) {
                 
@@ -65,6 +123,11 @@ class SET {
             
         });
         
+    }
+    private generateRandom(min: number, max: number) {  
+        return Math.floor(
+          Math.random() * (max - min) + min
+        );
     }
 }
 
