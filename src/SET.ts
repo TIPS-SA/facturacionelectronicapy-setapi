b6350b9c2cc61d8ab07c34a7dc14c061e49a684f
Marcos Jara <adrianjara@gmail.com>
2021-10-11 11:28:50 -0300

    Correccion envio Lote

--- src/SET.ts
+++ src/SET.ts
@@ -3,6 +3,7 @@
 import JSZip from "jszip";
 import pkcs12 from "./PKCS12";
 import fs from 'fs'; 
+import xml2js from 'xml2js'; 
 
 const https = require('https');
 const axios = require('axios');
@@ -38,72 +39,92 @@ class SET {
      * @param xmls 
      * @returns 
      */
-    async recibeLote(id: number, env: "test" | "prod", xmls: string[]) : Promise<any>{
-        if (xmls.length == 0) {
-            throw new Error("No se envió datos en el array de Documentos electrónicos XMLs");   
-        }
-        if (xmls.length > 50) {
-            throw new Error("Sólo se permiten un máximo de 50 Documentos electrónicos XML por lote");
-        }
+    recibeLote(id: number, env: "test" | "prod", xmls: string[]) : Promise<any>{
 
-        let url = 'https://sifen.set.gov.py/de/ws/async/recibe-lote.wsdl?wsdl';
-        if (env == "test") {
-            url = 'https://sifen-test.set.gov.py/de/ws/async/recibe-lote.wsdl?wsdl';
-        }
-
-        if (!this.cert) {
-            throw new Error("Antes debe Autenticarse");
-        }
-
-        if (!this.key) {
-            throw new Error("Antes debe autenticarse");
-        }
-
-        const zip = new JSZip();
-
-        for (let i = 0; i < xmls.length; i++) {
-            const xml = xmls[i];
-            zip.file(`xml_${i+1}`, xml);
-        }
+        return new Promise( async (resolve, reject) => {
+            try {
+                if (xmls.length == 0) {
+                    throw new Error("No se envió datos en el array de Documentos electrónicos XMLs");   
+                }
+                if (xmls.length > 50) {
+                    throw new Error("Sólo se permiten un máximo de 50 Documentos electrónicos XML por lote");
+                }
         
-        const zipAsBase64 = await zip.generateAsync({ type: "base64" });
-        //fs.writeFileSync(__dirname + '/zipped.zip', zipAsBase64);
-
-        const httpsAgent = new https.Agent({
-            cert: Buffer.from(this.cert, 'utf8'),
-            key: Buffer.from(this.key, 'utf8')
+                let url = 'https://sifen.set.gov.py/de/ws/async/recibe-lote.wsdl?wsdl';
+                if (env == "test") {
+                    url = 'https://sifen-test.set.gov.py/de/ws/async/recibe-lote.wsdl?wsdl';
+                }
+        
+                if (!this.cert) {
+                    throw new Error("Antes debe Autenticarse");
+                }
+        
+                if (!this.key) {
+                    throw new Error("Antes debe autenticarse");
+                }
+        
+                const zip = new JSZip();
+        
+                let rLoteDEXml = `<rLoteDE>\n`;
+                for (let i = 0; i < xmls.length; i++) {
+                    const xml = xmls[i].split('\n').slice(1).join('\n');    //Retirar xml
+                    
+                    rLoteDEXml += `${xml}\n`;
+                }
+                rLoteDEXml += `</rLoteDE>`;
+                //console.log("Enviar: " + rLoteDEXml);
+        
+                zip.file(`xml_file.xml`, `<?xml version="1.0" encoding="UTF-8"?>\n${rLoteDEXml}`);
+                
+                const zipAsBase64 = await zip.generateAsync({ type: "base64" });
+                //fs.writeFileSync(__dirname + '/zipped.zip', zipAsBase64);
+        
+                const httpsAgent = new https.Agent({
+                    cert: Buffer.from(this.cert, 'utf8'),
+                    key: Buffer.from(this.key, 'utf8')
+                });
+        
+                //axios.get(`${url}`, { httpsAgent }).then((respuesta: any) => {
+                    //console.log("respuesta", respuesta.data);
+        
+        
+                let xmlData=`<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">\n\
+                            <soap:Header/>\n\
+                            <soap:Body>\n\
+                                <rEnvioLote xmlns="http://ekuatia.set.gov.py/sifen/xsd">\n\
+                                    <dId>${id}</dId>\n\
+                                    <xDE>${zipAsBase64}</xDE>\n\
+                                </rEnvioLote>\n\
+                                ${rLoteDEXml}
+                            </soap:Body>\n\
+                        </soap:Envelope>\n`;
+                //console.log(xmlData);
+                axios.post(`${url}`, xmlData, {headers:
+                    {'Content-Type': 'text/xml'}, 
+                    httpsAgent 
+                }).then((respuestaSuccess: any) => {
+        
+                    console.log(respuestaSuccess.data);
+                }).catch((err:any) => {
+                    if (err && err.response && err.response.data) {
+                        var xmlResponse = err.response.data;
+                        var parser = new xml2js.Parser({explicitArray: false});
+        
+                        parser.parseStringPromise(xmlResponse).then(function (result) {
+                            resolve(result['env:Envelope']['env:Body']['ns2:rRetEnviDe']['ns2:rProtDe']);
+                        })
+                        .catch(function (err) {
+                            throw err;
+                        });
+                    } else {
+                        throw err;
+                    }
+                });
+                
+            } catch (error) {
+                reject(error);
+            }
         });
-
-        axios.get(`${url}`, { httpsAgent }).then((respuesta: any) => {
-            //console.log("respuesta", respuesta.data);
-
-
-            let xmls=`<soapenv:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">\n\
-                        <soapenv:Header/>\n\
-                        <soapenv:Body>\n\
-                            <rEnvioLote xmlns="http://ekuatia.set.gov.py/sifen/xsd">\n\
-                                <dId>${id}</dId>\n\
-                                <xDE>${zipAsBase64}</xDE>\n\
-                            </rEnvioLote>\n\
-                        </soapenv:Body>\n\
-                    </soapenv:Envelope>`;
-            console.log(xmls);
-            axios.post(`${url}`, xmls, { 
-                httpsAgent 
-            }).then((respuesta2: any) => {
-
-                console.log(respuesta2.data);
-            });
-        });
-
-        
-        return new Promise(e=>{});
-        
-    }
-    private generateRandom(min: number, max: number) {  
-        return Math.floor(
-          Math.random() * (max - min) + min
-        );
     }
 }
 
